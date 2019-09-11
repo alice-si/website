1f7475028b5aaa73f357853bc9214e1cace82b5d
alexAlice <alex@alice.si>
2019-09-12 01:19:59 +0200

    website version updated

---
+++ docs/js/aliceManagement.min.js
@@ -0,0 +1,31 @@
+var aliceManagementItems = document.querySelectorAll('.alice-management-item');
+var aliceManagementImages = document.querySelectorAll('.management-image-wrapper');
+
+// OnClick behaviour
+function onManagementClick(index) {
+  var aliceManagementItem = aliceManagementItems[index];
+  var aliceManagementImage = aliceManagementImages[index];
+
+  for (var i = 0; i < aliceManagementItems.length; i++) {
+    aliceManagementItems[i].classList.remove('active');
+    aliceManagementImages[i].classList.remove('active');
+  }
+  aliceManagementItem.classList.add('active');
+  aliceManagementImage.classList.add('active');
+}
+
+// Automated clicking
+var curManagementIndex = 0;
+var clickerInterval = setInterval(function() {
+  onManagementClick(curManagementIndex);
+  curManagementIndex = (curManagementIndex + 1) % aliceManagementItems.length;
+}, 3500);
+
+// Manual clicking handling
+for (let i = 0; i < aliceManagementItems.length; i++) {
+  var aliceManagementItem = aliceManagementItems[i];
+  aliceManagementItem.addEventListener('click', function () {
+    clearInterval(clickerInterval);
+    onManagementClick(i);
+  });
+}
